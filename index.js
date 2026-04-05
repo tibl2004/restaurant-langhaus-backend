af2d54a732a9ee79e3d23443da9a0d5f50bd030d
Timo Blumer <timo.blumer@gewa.ch>
2026-04-05 17:18:17 +0200

    jlsjfskf

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+app.set("trust proxy", 1); // 🔥 WICHTIG für Render/Vercel
 /*
 ========================
 SECURITY MIDDLEWARE
